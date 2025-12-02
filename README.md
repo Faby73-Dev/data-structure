6dae0ab728eadb89708663dc66619bfe79d7d65d
Faby73 <225463427+Faby73-Dev@users.noreply.github.com>
2025-12-02 18:49:57 -0300

    estructuras de datos con diferentes lenguajes de programacion

---
+++ README.md
@@ -0,0 +1,43 @@
+# Estructura de datos
+
+											/
+											| Boolean
+							/				|
+							|    Simples	| Char
+							|       o 		/ 
+				/			| Fundamentales \ Integer
+				|			|				| 
+				|			|				| Real
+				|			|				\
+				|			/
+				| Estaticas \
+				|			|
+				|			|				/
+				|			|				| Arreglo
+				|			|				|
+				|			|  Compuestas	| Conjuntos
+				|			|      o 		/
+				|			| Extructurdas 	\ Registros
+				|			|				|
+ 	Estrucura	|			\				| Archivos
+	   de		|							\
+	  Datos		|
+ 				|			/
+				|			| Pila
+				|			|
+				|			| Cola
+				|			|
+				| Dinamicas / Lista
+				| 			\
+				| 			| Arboles
+				|			|
+				\			| Grafos
+							\
+
+#### estructura: arreglo
+
+##### 1_ Diseñe un procedimiento recursivo que muestre el contenido de un vector de caracteres de 30 elementos.
+archivo: array-structure-one.cpp
+##### 2_ Diseñe una funcion recursiva que sume los elementos de un vector de caractes de 50 pcisiciones.
+
+##### 3_ Diseñe un procedimiento recursivo que muestre solo los cloares impares almacenado en un vector de enteros de 100 pocisiciones.
---
+++ language-c++/array-structure-one.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+//prototipo
+void cargarArreglo(int arreglo[],int);
+void mostrarArregloRecursivoDesendente(int arreglo[],int);
+void mostrarArregloRecursivoAsendente(int arreglo[],int);
+void clearPantalla();
+
+
+int main(){
+	clearPantalla();
+	int indice = 30;
+	int arreglo[indice];
+
+	cout << "-----------Carga de valores enteros---------" << endl;	
+	cargarArreglo(arreglo,indice);
+	clearPantalla();
+
+	cout << "-----Datos mostrados de manera recursiva (posicion desendente)----" << endl;	
+	mostrarArregloRecursivoDesendente(arreglo,indice);
+
+	cout << "-----Datos mostrados de manera recursiva (posicion asendente)----" << endl;	
+	mostrarArregloRecursivoAsendente(arreglo,indice);
+
+	//pantalla en espera
+	system("pause");
+
+	return 0;
+}
+
+//definicion de funcion
+void cargarArreglo(int arreglo[], int i){
+	for(int j = 0;j <= i; j++){
+		cout << "ingrese valor entero: " << endl;	
+		cin >> arreglo[j];
+	}
+
+}
+
+void mostrarArregloRecursivoDesendente(int arreglo[],int i){
+	if( i == 0){
+		cout << " pocision " << i << " el valor es : " << arreglo[i] << endl;
+	}else{
+		cout << " pocision " << i << " el valor es : " << arreglo[i] << endl;
+		i = i - 1;
+		//llamada de funcion recursiva
+		mostrarArregloRecursivoDesendente(arreglo,i);
+		
+	}
+
+}
+
+void mostrarArregloRecursivoAsendente(int arreglo[],int i){
+	if(i == 0){
+		cout << " pocision " << i << " el valor es : " << arreglo[i] << endl;
+	}else{
+		i = i - 1;
+		mostrarArregloRecursivoAsendente(arreglo,i);
+		cout << " pocision " << i+1 << " el valor es : " << arreglo[i+1] << endl;
+		
+	}
+
+}
+
+//limpia pantalla
+void clearPantalla(){
+	system("cls");
+
+}
